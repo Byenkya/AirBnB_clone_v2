b67df93729464523a54fb3f9fac91a6c441657db
Byenkya <jbyenkya@idi.co.ug>
2022-09-13 13:53:17 +0300

    update state.py

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
 
     if os.getenv('HBNB_TYPE_STORAGE') != "db":
         @property
-        def cities(self):
+        def all_cities(self):
             """
             getter for cities
             """
