e00f19183984cc87b9495b4642ac8a667766ac24
Byenkya <51961339+Byenkya@users.noreply.github.com>
2022-09-13 11:43:17 +0300

    update user.py

--- models/user.py
+++ models/user.py
@@ -13,6 +13,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", back_populates="user", cascade="delete")
-    reviews = relationship("Review", back_populates="user", cascade="delete")
