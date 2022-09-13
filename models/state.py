4fe4ff779773e1addeba53265877466c2786a14d
Byenkya <jbyenkya@idi.co.ug>
2022-09-13 13:47:02 +0300

    changes

--- models/state.py
+++ models/state.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+# from models import storage
+# from models import City
+import os
 
+class State(BaseModel, Base):
+    """This is the class for State"""
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    __tablename__ = 'states'
 
-# #!/usr/bin/python3
-# """This is the state class"""
-# from models.base_model import BaseModel, Base
-# from sqlalchemy import Column, String, Integer
-# from sqlalchemy.orm import relationship
-# # from models import storage
-# # from models import City
-# import os
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state', cascade='delete')
 
-# class State(BaseModel, Base):
-#     """This is the class for State"""
-
-#     __tablename__ = 'states'
-
-#     name = Column(String(128), nullable=False)
-#     cities = relationship('City', back_populates='state', cascade='delete')
-
-#     if os.getenv('HBNB_TYPE_STORAGE') != "db":
-#         @property
-#         def cities(self):
-#             """
-#             getter for cities
-#             """
-#             cities = []
-#             insta = storage.all(City)
-#             for value in insta.values():
-#                 if value.state_id == self.id:
-#                     cities.append(value)
-#             return cities
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def cities(self):
+            """
+            getter for cities
+            """
+            cities = []
+            insta = storage.all(City)
+            for value in insta.values():
+                if value.state_id == self.id:
+                    cities.append(value)
+            return cities
