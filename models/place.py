32ebec199d70795a2810a0c42f552ceff153d77b
Byenkya <jbyenkya@idi.co.ug>
2022-10-15 20:09:24 +0300

    updatessss

--- models/place.py
+++ models/place.py
@@ -27,6 +27,7 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     if storage_type == 'db':
+        id = Column(String(60), primary_key=True)
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
