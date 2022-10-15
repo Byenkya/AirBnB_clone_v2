7f3a04f46cc4cfb80b2d663e8cf1e8536374aa30
Byenkya <jbyenkya@idi.co.ug>
2022-10-15 21:35:44 +0300

    testing

--- models/place.py
+++ models/place.py
@@ -12,11 +12,11 @@ from sqlalchemy.orm import relationship
 
 if storage_type == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
+                          Column('place_id',
                                  ForeignKey('places.id'),
                                  primary_key=True,
                                  nullable=False),
-                          Column('amenity_id', String(60),
+                          Column('amenity_id',
                                  ForeignKey('amenities.id'),
                                  primary_key=True,
                                  nullable=False)
