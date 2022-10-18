c5c3f31aa80eefc0dd08caba0138201d9a61dc82
Byenkya <51961339+Byenkya@users.noreply.github.com>
2022-10-18 09:30:56 +0300

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,76 +12,20 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """
-        defines what to return on 
-    """
-    return "Hello HBNB!"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """
-     route plus text specified
-     """
-    text = text.replace('_', ' ')
-    return ("C {}".format(text))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythonisfun(text="is cool"):
-    """
-        python route plus default text
-    """
-    text = text.replace('_', ' ')
-    return ("Python {}".format(text))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def numberOnly(n):
-    """
-        number int routes only if end is
-    """
-    return ("{} is a number".format(n))
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def numberTemplate(n):
-    """
-        render template if conditions are met
-    """
-    return (render_template('5-number.html', n=n))
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_even(n):
-    """
-        render template if conditions are met
-    """
-    return (render_template('6-number_odd_or_even.html', n=n))
-
-
-@app.route('/states_list/', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
     """
-        show all states
-    """
-    dict2 = {}
-    for k, v in storage.all(State).items():
-        dict2.update({v.name: v.id})
-    print(dict2)
-    return (render_template('7-states_list.html', dict2=dict2))
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def shutdown_session(exception=None):
-    """
-        end session of db
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
